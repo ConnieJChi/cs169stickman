add59b279a93efba23ae141e5a498c7e3cad7d70
ConnieJChi <conniejchi@berkeley.edu>
2020-09-10 20:35:57 -0700

    will rewind this later

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -8,78 +8,71 @@ class HangpersonGame
   # def initialize()
   # end
 
-  attr_accessor :word
-  attr_accessor :guesses
-  attr_accessor :wrong_guesses
-
+  attr_accessor :word, :guesses, :wrong_guesses
 
   def initialize(word)
     @word = word
-    @guesses = ""
-    @wrong_guesses = ""
-    @guess_list = []
-    @wrong_guess_list = []
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
-  def guess(word)
-    if word == "" or word == nil
-       raise ArgumentError, 'Argument is an empty string'
-    elsif word =~ /[^a-zA-Z]/
-        raise ArgumentError, 'Argument is not a letter'
-    end
-
-    word = word.downcase
-
-    if @guess_list.include? word or @wrong_guess_list.include? word
-        return false
-    end
-
-    if @word.include? word
-        @guesses = word
-        @guess_list.append(word)
-        return true
-    else
-        @wrong_guesses = word
-        @wrong_guess_list.append(word)
-        return true
-    end
-  end
-
+  # displays the formatted word after guesses have been made
   def word_with_guesses
-      ret_str = ""
-      @word.split("").each do |c|
-          if @guess_list.include? c
-              ret_str = ret_str + c
-          else
-              ret_str = ret_str + "-"
-          end
+    result = ''
+    @word.split('').each do |char|
+      if @guesses.include? char
+        result << char
+      else
+        result << '-'
       end
+    end
 
-      return ret_str
+    return result
   end
 
   def check_win_or_lose
-      if word_with_guesses == @word
-          return :win
-      elsif @wrong_guess_list.length >= 7
-          return :lose
-      else
-          return :play
-      end
+    if word_with_guesses.downcase == @word.downcase
+      return :win
+    elsif @wrong_guesses.length >= 7
+      return :lose
+    else
+      return :play
+    end
   end
 
-  # You can test it by running $ bundle exec irb -I. -r app.rb
-  # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
-  #  => "cooking"   <-- some random word
+  # make a guess as to what the word is
+  def guess(letter)
+    # make sure the letter is actually a letter
+    if letter == nil || !(letter.class == String && letter =~ /^[A-z]$/i)
+      raise ArgumentError
+    end
+
+    # handle different cases
+    letter.downcase!
+
+    # handle repeated guesses
+    if @guesses.include?(letter) || @wrong_guesses.include?(letter)
+      return false
+    end
+
+    #handle case where guess is not a letter
+    return false if letter.length != 1
+
+    # finally handle check and return true
+    if @word.include? letter
+      @guesses << letter
+    else
+      @wrong_guesses << letter
+    end
+    return true
+  end
+
   def self.get_random_word
     require 'uri'
     require 'net/http'
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
-    Net::HTTP.new('watchout4snakes.com').start { |http|
-      return http.post(uri, "").body
-    }
+    Net::HTTP.post_form(uri ,{}).body
   end
-
 
 
 end
